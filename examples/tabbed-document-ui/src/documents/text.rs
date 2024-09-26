58fd03230472485239b5c31e77bedef2ea2e8ce2
Dominic Clifton <me@dominicclifton.name>
2024-09-26 13:32:06 +0200

    Use an editor instead of a label for the content.  Use TupleStackExt when creating v_stacks and h_stacks.

--- examples/tabbed-document-ui/src/documents/text.rs
+++ examples/tabbed-document-ui/src/documents/text.rs
@@ -1,16 +1,14 @@
 use std::fs;
 use std::path::PathBuf;
-use floem::event::EventListener;
 use floem::peniko::Color;
-use floem::reactive::{create_rw_signal, RwSignal, SignalGet};
+use floem::reactive::SignalGet;
 use floem::style::TextOverflow;
 use floem::View;
-use floem::views::{Decorators, h_stack, label, static_label, v_stack};
+use floem::views::{Decorators, label, static_label, text_editor, TupleStackExt};
 
 pub struct TextDocument {
     path: PathBuf,
     content: String,
-    selection: RwSignal<Option<(usize, usize)>>,
 }
 
 impl TextDocument {
@@ -21,77 +19,67 @@ impl TextDocument {
         Self {
             path,
             content,
-            selection: create_rw_signal(None),
         }
     }
 
     pub fn build_view(&self) -> impl View {
-        h_stack((
-            //
-            // info panel
-            //
-            v_stack((
-                {
-                    h_stack((
-                        static_label("path"),
-                        static_label(self.path.to_str().unwrap())
-                            .style(|s|s
-                                // FIXME doesn't make any difference, path appears truncated
-                                .text_overflow(TextOverflow::Wrap)
-                            )
-                    ))
-                },
-                {
-                    h_stack((
-                        static_label("selection"),
-                        {
-                            let selection = self.selection.clone();
-                            label(move || {
-                                selection.get()
-                                    .map_or_else(
-                                        ||"None".to_string(),
-                                        |(offset, length)|{
-                                            format!("offset: {}, length: {}", offset, length)
-                                        }
-                                    )
-                            })
-                        }
-                    ))
-                }
-            ))
-                .style(|s|s
-                    .height_full()
-                    .width_pct(20.0)
-                ),
+        let editor = text_editor(self.content.clone());
+        let cursor = editor.editor().cursor;
 
-            //
-            // content
-            //
+        let info_panel = (
             {
-                // FIXME this needs to be reactive
-                let content_label = format!("{}", self.content);
-                label(move || {
-                    // FIXME do we really have to clone here? it's expensive and memory inefficient on a large file!
-                    content_label.clone()
+                (
+                    static_label("path"),
+                    static_label(self.path.to_str().unwrap())
+                        .style(|s| s
+                            // FIXME doesn't make any difference, path appears truncated
+                            .text_overflow(TextOverflow::Wrap)
+                        )
+                )
+                    .h_stack()
+            },
+            {
+                (
+                    static_label("selection"),
+                    {
+                        label(move || {
+                            let cursor = cursor.get();
+                            let selection = cursor.get_selection();
 
-                    // FIXME text selection doesn't work properly on this element, there is strange behavior!
-                })
-                    .on_event_cont(EventListener::PointerUp,|event|{
-                        println!("{:?}", event);
-                        // TODO somehow see what text is now selected in the label, get and store the selection offset and length
-                        //      in `self.selection`
-                        let selection = (0,0);
-
-                        // FIXME this doesn't compile: `self` escapes the method body here
-                        //self.selection.set(Some(selection));
-                    })
+                            selection
+                                .map_or_else(
+                                    || "None".to_string(),
+                                    |(start, end)| {
+                                        format!("offset: {}, length: {}", start, (end as i32 - start as i32).abs())
+                                    }
+                                )
+                        })
+                    }
+                )
+                    .h_stack()
             }
-                .style(|s|s
-                    .height_full()
-                    .width_full()
-                    .background(Color::DARK_GRAY)
-                ),
-        ))
+        )
+            .v_stack()
+            .style(|s| s
+                .height_full()
+                .width_pct(20.0)
+            );
+
+        let content = {
+            editor
+        }
+            .style(|s| s
+                .height_full()
+                .width_full()
+                .background(Color::DARK_GRAY)
+            );
+
+
+        (
+            info_panel,
+            content,
+        )
+            .h_stack()
             .style(|s|s
                 .width_full()
                 .height_full()
