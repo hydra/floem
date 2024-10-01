aee0a451aa41c0ae9cb8a2c7ed35d79886b35d67
Dominic Clifton <me@dominicclifton.name>
2024-10-01 11:44:54 +0200

    Use info panel on image document.

--- examples/tabbed-document-ui/src/documents/image.rs
+++ examples/tabbed-document-ui/src/documents/image.rs
@@ -2,7 +2,8 @@ use std::path::PathBuf;
 use floem::peniko::Color;
 use floem::style::TextOverflow;
 use floem::View;
-use floem::views::{Decorators, h_stack, img_from_path, label, static_label, v_stack};
+use floem::views::{Decorators, img_from_path, label, static_label, TupleStackExt};
+use crate::ui::info_panel::{info_panel, info_panel_row};
 
 pub struct ImageDocument {
     path: PathBuf,
@@ -20,56 +21,53 @@ impl ImageDocument {
     }
 
     pub fn build_view(&self) -> impl View {
-        h_stack((
-            //
-            // info panel
-            //
-            v_stack((
-                {
-                    h_stack((
-                        static_label("path"),
-                        static_label(self.path.to_str().unwrap())
-                            .style(|s|s
-                                // FIXME doesn't make any difference, path appears truncated
-                                .text_overflow(TextOverflow::Wrap)
-                            )
-                    ))
-                },
-                {
-                    h_stack((
-                        static_label("coordinate"),
-                        {
-                            // FIXME this needs to be reactive
-                            let coordinate_label = format!("{:?}", self.coordinate);
-                            label(move || coordinate_label.clone())
-                        }
-                    ))
-                }
-            ))
-                .style(|s|s
-                    .height_full()
-                    .width_pct(20.0)
-                ),
 
-            //
-            // content
-            //
-            {
-                // TODO show the image
-                let path = self.path.clone();
-                img_from_path(move || path.clone())
+        let info_panel = info_panel((
+            info_panel_row(
+                "path",
+                static_label(self.path.to_str().unwrap())
                     .style(|s|s
-                        .min_height(256)
-                        .min_width(256)
+                        // FIXME doesn't make any difference, path appears truncated
+                        .text_overflow(TextOverflow::Wrap)
+                        // FIXME this doesn't work either
+                        //.text_clip()
                     )
-            }
-                .style(|s|s
-                    .height_full()
-                    // FIXME if this is 80% or 'full' it still doesn't take up the remaining space.
-                    .width_full()
-                    .background(Color::PURPLE)
-                ),
+            ),
+            info_panel_row(
+                "coordinate",
+                {
+                    // FIXME this needs to be reactive
+                    let coordinate_label = format!("{:?}", self.coordinate);
+                    label(move || coordinate_label.clone())
+                }
+            ),
         ))
+            .style(|s|s
+                .height_full()
+                .width_pct(20.0)
+            );
+
+        let content = {
+            // TODO show the image
+            let path = self.path.clone();
+            img_from_path(move || path.clone())
+                .style(|s|s
+                    .min_height(256)
+                    .min_width(256)
+                )
+        }
+            .style(|s|s
+                .height_full()
+                // FIXME if this is 80% or 'full' it still doesn't take up the remaining space.
+                .width_full()
+                .background(Color::PURPLE)
+            );
+
+        (
+            info_panel,
+            content,
+        )
+            .h_stack()
             .style(|s|s
                 .width_full()
                 .height_full()
